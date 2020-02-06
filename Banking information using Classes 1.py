4008bb72c073c807c9e9a1dea24510eba71088fd
devanshsolani <53579366+devanshsolani@users.noreply.github.com>
2020-02-06 10:03:41 +0530

    Add files via upload

---
+++ Banking information using Classes 1.py
@@ -0,0 +1,25 @@
+print("Welcome to the IndianSwiss")
+class banking:
+    def __init__(self):
+        custName = input("Enter the customer name:")
+        self.amount=5000
+        print("The Amount you have in your A/C is 5000")
+
+    def withdraw(self):
+        self.withdraw=int(input("To be withdrawn:"))
+        print("The amount to want to withdraw is:",self.withdraw)
+        self.amount=self.amount-self.withdraw
+        return self.amount
+
+    def deposit(self):
+        self.deposit=int(input("To be deposited:"))
+        print("The amount to want to deposit is:",self.deposit)
+        self.amount=self.amount+self.deposit
+        return self.amount
+    
+    
+
+for i in range(2):
+    b1=banking()
+    print(b1.withdraw())
+    print(b1.deposit())
---
+++ Banking information using Classes 2.py
@@ -0,0 +1,32 @@
+class Bank(object):
+    name=""
+    acctno=0
+    initial_bal=0.0
+    amt=0
+
+    def accept(self):
+        print("Enter name, Acctno,and Initial Balance:")
+        self.name=input()
+        self.acctno=int(input())
+        self.initial_bal=float(input())
+        print("Customer Details")
+        print(self.name)
+        print(self.acctno)
+        print(self.initial_bal)
+
+    def deposit(self):
+        print("Enter amount to be deposited:")
+        self.amt=int(input())
+        self.initial_bal=self.initial_bal+self.amt
+        print("Updated balance is", self.initial_bal)
+
+    def withdraw(self):
+        print("Enter amount to be withdrawn:")
+        self.amt=int(input())
+        self.initial_bal=self.initial_bal-self.amt
+        print("Updated balance is", self.initial_bal)
+
+b1=Bank()
+b1.accept()
+b1.withdraw()
+b1.deposit()
