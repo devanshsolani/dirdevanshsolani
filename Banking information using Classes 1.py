9b0487f774815ebdca30126105118cbaeac0f645
devanshsolani <53579366+devanshsolani@users.noreply.github.com>
2020-02-06 10:20:33 +0530

    Update Banking information using Classes 1.py

--- Banking information using Classes 1.py
+++ Banking information using Classes 1.py
@@ -2,24 +2,23 @@ print("Welcome to the IndianSwiss")
 class banking:
     def __init__(self):
         custName = input("Enter the customer name:")
-        self.amount=5000
+        self.amount = 5000
         print("The Amount you have in your A/C is 5000")
-
+        
     def withdraw(self):
-        self.withdraw=int(input("To be withdrawn:"))
-        print("The amount to want to withdraw is:",self.withdraw)
-        self.amount=self.amount-self.withdraw
+        self.withdraw = int(input("To be withdrawn: "))
+        print("The amount to want to withdraw is:", self.withdraw)
+        self.amount = self.amount - self.withdraw
         return self.amount
-
+    
     def deposit(self):
-        self.deposit=int(input("To be deposited:"))
-        print("The amount to want to deposit is:",self.deposit)
-        self.amount=self.amount+self.deposit
+        self.deposit = int(input("To be deposited:"))
+        print("The amount to want to deposit is:", self.deposit)
+        self.amount = self.amount+self.deposit
         return self.amount
     
     
-
 for i in range(2):
-    bankuser1=banking()
+    bankuser1 = banking()
     print(bankuser1.withdraw())
     print(bankuser1.deposit())
