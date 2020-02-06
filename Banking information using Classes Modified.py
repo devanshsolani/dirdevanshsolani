2e5d2491c4330b68097a617ea63ab4ff8b7da825
devanshsolani <53579366+devanshsolani@users.noreply.github.com>
2020-02-06 10:22:30 +0530

    Update Banking information using Classes Modified.py

--- Banking information using Classes Modified.py
+++ Banking information using Classes Modified.py
@@ -1,14 +1,15 @@
 class Bank(object):
-    name=""
-    acctno=0
-    initial_bal=0.0
-    amt=0
+    name = ""
+    acctno = 0
+    initial_bal = 0.0
+    amt = 0
 
     def accept(self):
         print("Enter name, Acctno,and Initial Balance:")
-        self.name=input()
-        self.acctno=int(input())
-        self.initial_bal=float(input())
+        self.name = input()
+        self.acctno = int(input())
+        self.initial_bal = float(input())
+        
         print("Customer Details")
         print(self.name)
         print(self.acctno)
@@ -16,17 +17,18 @@ class Bank(object):
 
     def deposit(self):
         print("Enter amount to be deposited:")
-        self.amt=int(input())
-        self.initial_bal=self.initial_bal+self.amt
+        self.amt = int(input())
+        self.initial_bal = self.initial_bal + self.amt
         print("Updated balance is", self.initial_bal)
 
     def withdraw(self):
         print("Enter amount to be withdrawn:")
-        self.amt=int(input())
-        self.initial_bal=self.initial_bal-self.amt
+        self.amt = int(input())
+        self.initial_bal = self.initial_bal - self.amt
         print("Updated balance is", self.initial_bal)
 
-b1=Bank()
+        
+b1 = Bank()
 b1.accept()
 b1.withdraw()
 b1.deposit()
